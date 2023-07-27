da75d6437f5f0cf312e7af5706b4e45abb4891b3
Random228 <ranbom89@gmail.com>
2023-07-27 19:03:46 +0300

    commit

--- Install.sh
+++ Install.sh
@@ -1,6 +1,27 @@
 #!/bin/bash
 
+function START(){
 docker pull registry.red-soft.ru/ubi7/ubi-micro:latest
 dir=$( docker image inspect registry.red-soft.ru/ubi7/ubi-micro:latest | grep "diff" | awk {'print $2'} | sed 's/.$//' | sed 's/.$//' | sed 's/^.//' )
 yum --installroot="$dir" --releasever=/ --setopt=tsflags=nodocs --setopt=group_package_types=mandatory -y install nginx-1.18.0-1.el7
 yum --installroot="$dir" -y clean all
+}
+
+function CREATE_CONTAINER(){
+    if [[ -d /home/docker/ ]]; then
+    wget -c --directory-prefix=/home/docker/ https://raw.githubusercontent.com/Random228/TestFoRedOS/NewTest/nginx.conf
+    wget -c --directory-prefix=/home/docker/ https://raw.githubusercontent.com/Random228/TestFoRedOS/NewTest/Dockerfile
+    docker vuild -t "registry.red-soft.ru/ubi7/nginx-micro:test" /home/docker/Dockerfile
+    fi
+}
+
+function RUN(){
+    docker run -d -p 8080:80 registry.red-soft.ru/ubi7/nginx-micro:test 
+}
+
+function MANI(){
+    START
+    CREATE_CONTAINER
+    RUN
+}
+MANI
